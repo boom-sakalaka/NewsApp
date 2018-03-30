747d765c4ba8e941ae1688af436ce7a39634100e
GU <936285702@qq.com>
2018-03-30 13:32:23 +0800

    修改样式

--- src/components/register.vue
+++ src/components/register.vue
@@ -11,8 +11,8 @@
       <mt-field label="邮箱" placeholder="请输入邮箱" type="email" v-model="email"></mt-field>
       <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
       <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
-      <mt-field label="网站" placeholder="请输入网址" type="url" v-model="website"></mt-field>
-      <mt-field label="数字" placeholder="请输入数字" type="number" v-model="number"></mt-field>
+      <!--<mt-field label="网站" placeholder="请输入网址" type="url" v-model="website"></mt-field>-->
+      <!--<mt-field label="数字" placeholder="请输入数字" type="number" v-model="number"></mt-field>-->
       <mt-field label="生日" placeholder="请输入生日" type="date" v-model="birthday"></mt-field>
       <mt-field label="自我介绍" placeholder="自我介绍" type="textarea" rows="4" v-model="introduction"></mt-field>
       <div class="ensure">
--- src/components/user.vue
+++ src/components/user.vue
@@ -7,12 +7,13 @@
       <mt-button icon="more" slot="right"></mt-button>
     </mt-header>
     <div class="user-img">
-      <div class="head-img"></div>
+      <div class="head-img" @click="sheetVisible = true"></div>
       <div class="user-name">V仔兽</div>
     </div>
     <div class="user-opration-list">
       <mt-cell title="我的收藏"  is-link>
         <img slot="icon" src="../assets/star.png" width="24" height="24">
+        <mt-badge size="normal" type="error">10</mt-badge>
       </mt-cell>
       <mt-cell title="我的信息"  is-link>
         <img slot="icon" src="../assets/change.png" width="24" height="24">
@@ -27,11 +28,16 @@
       <split></split>
       <mt-cell title="游戏"  is-link>
         <img slot="icon" src="../assets/game.png" width="24" height="24">
+        <mt-badge size="normal" type="error">8</mt-badge>
       </mt-cell>
       <mt-cell title="设置"  is-link>
         <img slot="icon" src="../assets/setting.png" width="24" height="24">
       </mt-cell>
+      <mt-cell title="夜间模式">
+        <mt-switch></mt-switch>
+      </mt-cell>
     </div>
+    <mt-actionsheet :actions="actions" v-model="sheetVisible"></mt-actionsheet>
     <!--<tabbar :selected="selected" :tabs='tabs'></tabbar>-->
     <footer-nav></footer-nav>
   </div>
@@ -44,27 +50,43 @@ import { Indicator } from 'mint-ui';
   import footerNav from '../components/footer'
   export default {
     name: 'userContent',
+    data() {
+      return {
+          actions: [],
+          sheetVisible: false,
+          value3: '夜间模式'
+      }
+    },
     beforeCreate() {
       Indicator.open()
       setTimeout(()=> {
         Indicator.close()
       },2000)
     },
-    data () {
-      return {
-//        selected:"UserCenter",
-//        tabs:[require("../assets/news.png"),require("../assets/hotnews.png"),
-//          require("../assets/user.png")],
-      }
-    },
     methods: {
       changeIn() {
         window.history.go(-1);
-      }
+      },
+      takePhoto() {
+        console.log('taking photo');
+      },
+
+      openAlbum() {
+        console.log('opening album');
+      },
     },
     components: {
       split,
       footerNav
+    },
+    mounted() {
+      this.actions = [{
+        name: '拍照',
+        method: this.takePhoto
+      }, {
+        name: '从相册中选择',
+        method: this.openAlbum
+      }]
     }
   }
 </script>
@@ -100,5 +122,4 @@ import { Indicator } from 'mint-ui';
       margin-left: -50px
   .user-opration-list
       /*margin: 30px 30px 0  30px*/
-      margin-top: 30px
 </style>
